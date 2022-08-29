ab1905fec5f7ef15faf6b2e4a958be529890a00e
VitorHenrique-ViniciusFaoro <VitorHenrique-ViniciusFaoro@teste.com>
2022-08-29 09:40:07 -0300

    Criacao do projeto

---
+++ src/test/java/br/com/etechoracio/pw2jpa/Pw2JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.pw2jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw2JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
