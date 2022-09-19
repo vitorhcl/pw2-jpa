1fa10ca790644f260c2dd929ece243abe062e74d
VitorHenrique-ViniciusFaoro <VitorHenrique-ViniciusFaoro@test.com>
2022-09-19 10:47:43 -0300

    Criacao das entidades Proprietario e Telefone

---
+++ src/main/java/br/com/etechoracio/jpa/entity/Telefone.java
@@ -0,0 +1,21 @@
+package br.com.etechoracio.jpa.entity;
+
+import javax.persistence.*;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name="TBL_TELEFONE")
+public class Telefone {
+	@Id
+	@Column(name="ID_TELEFONE")
+	private Long id;
+	
+	@Column(name="TX_AREA")
+	private String area;
+	
+	@Column(name="TX_NUMERO")
+	private String numero;
+}
