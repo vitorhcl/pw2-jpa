91fddc17d2db046467201fc979781999a185e54c
luiz-vitorhenrique <luiz-vitorhenrique@test.com>
2022-10-24 10:53:14 -0300

    Inclusao de entidades GrupoUsuario e Usuario

---
+++ src/main/java/br/com/etechoracio/jpa/entity/GrupoUsuario.java
@@ -0,0 +1,26 @@
+package br.com.etechoracio.jpa.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.*;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Table (name = "TBL_GRUPO_USUARIO")
+public class GrupoUsuario {
+	@Id
+	@Column(name = "ID_GRUPO_USUARIO")
+	private long id;
+	
+	@Column(name = "TX_NOME")
+	private String nome;
+	
+	@Column(name = "DT_INICIO_VIGENCIA")
+	private LocalDateTime dataInicioVigencia;
+	
+	@Column(name = "DT_FIM_VIGENCIA")
+	private LocalDateTime dataFimVigencia;
+}
---
+++ src/main/java/br/com/etechoracio/jpa/entity/Usuario.java
@@ -0,0 +1,30 @@
+package br.com.etechoracio.jpa.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table (name = "TBL_USUARIO")
+public class Usuario {
+	@Id
+	@Column(name = "ID_USUARIO")
+	private long id;
+	@Column(name = "TX_NOME")
+	private String nome;
+	@Column(name = "TX_SENHA")
+	private String senha;
+	@Column(name = "DT_INATIVACAO")
+	private LocalDateTime dataInativacao;
+	@ManyToMany
+	@JoinTable (name = "TBL_REL_USUARIO_GRUPO", 
+	joinColumns = @JoinColumn(name = "ID_USUARIO"),
+	inverseJoinColumns = @JoinColumn(name = "ID_GRUPO_USUARIO"))
+	private List <GrupoUsuario> grupos;
+}
